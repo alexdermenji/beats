27f382fcc7fbb5f746cb9f5028989792359f1feb
alexdermenji <allexdermenji@gmail.com>
2021-03-19 08:30:44 +0000

    modal menu open/close functionality

---
+++ js/script.js
@@ -0,0 +1,16 @@
+////// #MODAL MENU
+
+const hamburgerBtn = document.querySelector(".hamburger");
+const modalMenu = document.querySelector(".modal");
+const body = document.body;
+const hamburgerBtnClose = document.getElementById("modalMenuClose");
+
+hamburgerBtn.addEventListener("click", function () {
+  modalMenu.style.display = "flex";
+  body.style.overflow = "hidden";
+});
+
+hamburgerBtnClose.addEventListener("click", function () {
+  modalMenu.style.display = "none";
+  body.style.overflow = "";
+});
