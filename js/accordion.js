9de5c7246f72dabc2926ce864959c20d2acda43d
alexdermenji <allexdermenji@gmail.com>
2021-03-22 20:38:12 +0000

    triangles active class done

--- js/accordion.js
+++ js/accordion.js
@@ -5,11 +5,13 @@
   console.log(descriptionWrap);
   const description = document.querySelectorAll(".team__description");
   console.log(description);
+  const triangle = document.querySelectorAll(".team__triangle");
 
   function hideAccordeon() {
     for (let i = 0; i < descriptionWrap.length; i++) {
       descriptionWrap[i].style.height = "0px";
       descriptionWrap[i].classList.remove("team__description--active");
+      triangle[i].classList.remove("team__triangle--up");
     }
   }
 
@@ -17,6 +19,7 @@
     let height = getComputedStyle(description[element]).height;
     descriptionWrap[element].style.height = height;
     descriptionWrap[element].classList.add("team__description--active");
+    triangle[element].classList.add("team__triangle--up");
   }
 
   for (let i = 0; i < btn.length; i++) {
