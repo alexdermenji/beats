900656357a83dcbd04f1353cc1b1cb60510d8e8b
alexdermenji <allexdermenji@gmail.com>
2021-04-03 09:10:21 +0100

    mobile sizesl

--- js/modalmenu.js
+++ js/modalmenu.js
@@ -5,20 +5,6 @@
   const body = document.body;
   const hamburgerBtnClose = document.getElementById("modalMenuClose");
   const menuLinks = document.querySelectorAll(".menu__link");
-  const scrollLinks = document.querySelectorAll(".scroll__item");
-
-  /// scroll
-  scrollLinks.forEach((link) => {
-    link.addEventListener("click", function (e) {
-      scrollLinks.forEach((el) => {
-        el.classList.remove("scroll__item--active");
-      });
-      link.classList.add("scroll__item--active");
-      e.preventDefault();
-      const dataValue = e.target.dataset.scrollTo;
-      scrollToSection(dataValue);
-    });
-  });
 
   ///menu -links
   menuLinks.forEach((link) => {
@@ -26,13 +12,6 @@
       e.preventDefault();
       const dataValue = e.target.dataset.scrollTo;
       scrollToSection(dataValue);
-      const scrollBtn = document.querySelector(
-        `[data-scroll-to='${dataValue}']`
-      );
-      scrollLinks.forEach((el) => {
-        el.classList.remove("scroll__item--active");
-      });
-      scrollBtn.classList.add("scroll__item--active");
     });
   });
 
